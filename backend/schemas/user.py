21df7f306d679d8a210799452ff2a8aeb22e7e14
rumi <pelopsmrf@gmail.com>
2024-02-24 21:26:46 +0500

    add support for hashing

---
+++ backend/schemas/user.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, EmailStr, Field
+
+#properties required during user creation
+class UserCreate(BaseModel):
+    email : EmailStr
+    password : str = Field(..., min_length=4)
